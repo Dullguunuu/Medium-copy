6d1575641eac1a474fe26c1cc5f30a9c5ed335e0
Dullguunuu <dulguun.nst@gmail.com>
2023-01-11 12:31:46 +0800

    jan11

--- src/components/discover.js
+++ src/components/discover.js
@@ -1,15 +1,16 @@
 export default function Discover({ discoverMore }) {
     console.log(discoverMore)
     return (
-        <div>
-            <h3>Discover more of what matters to you</h3>
+        <div style={{ width: "400px", marginLeft: "100px" }}>
+            <h3 style={{ fontWeight: "700", fontSize: "11px", textTransform: "uppercase", marginBottom: "25px" }}>Discover more of what matters to you</h3>
             <div>
                 {
                     discoverMore.map((e) => (
-                        <button className="btn btn-secondary">{e}</button>
+                        <button className="btn btn-outline-secondary m-1" style={{ fontWeight: "400", fontSize: "13px" }}>{e}</button>
                     ))
                 }
             </div>
+            <hr style={{ marginTop: "25px" }}></hr>
         </div>
     )
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,12 @@
 export default function Footer({ footerLinks }) {
     return (
-        <div>
+        <div className="d-flex flex-wrap" style={{ width: "400px", marginLeft: "100px" }
+        }>
             {
                 footerLinks.map((e) => (
-                    <span>{e}</span>
+                    <span style={{ marginRight: "27px", fontWeight: "400", fontSize: "13px", color: "#757575" }}>{e}</span>
                 ))
             }
-        </div>
+        </div >
     )
 }
